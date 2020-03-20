f225bb9263667302f8e85c22c90cccd7d3ff015c
Nazmul999 <mdnazmulhuda1327@gmail.com>
2020-03-20 10:29:37 +0600

    use routing

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Sorry, this keyword is not found
+               404 error !!!!
+                
+            </h1>
+        </div>
+    );
+};
+
+export default NotFound;
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './products.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {  faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 const Products = (props) => {
-    const { img, name, seller, stock, price } = props.product;
+    const { img, name, seller, stock, price, key } = props.product;
     return (
         <div className="product">
             <div>
                 <img src={img} alt="" />
             </div>
             <div>
-                <h4 className="product-name">{name}</h4>
+                <h4 className="product-name"><Link to ={"/product/"+
+                key}>{name}</Link></h4>
                 <br />
                 <p><small>by:{seller}</small></p>
                 <p>${price}</p>
